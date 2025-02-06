1d74cb4c2afadc9939cb0cedbf7ac48f04f52816
Kartikeyy-pandeyy <kartikeyy.pandeyy@gmail.com>
2025-02-06 23:42:30 +0530

    updated ep

--- index.js
+++ index.js
@@ -6,18 +6,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.post("/calculate", async (req, res) => {
     const { num1, num2, operation } = req.body;
     const backendUrl = operation === "add" 
-        ? "http://calculator-production-e65e.up.railway.app/add" 
-        : "http://calculator-production-e65e.up.railway.app/subtract";
+        ? "https://calculator-production-e65e.up.railway.app/add" 
+        : "https://calculator-production-e65e.up.railway.app/subtract";
 
-    try {
-        const response = await axios.post(backendUrl, { num1, num2 });
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).json({ error: "Service Unavailable" });
-    }
-});
-
-app.listen(5000, () => console.log("API Gateway running on port 5000"));
+        try {
+            const response = await axios.post(backendUrl, { num1, num2 });
+            res.json(response.data);
+        } catch (error) {
+            console.error("Error Details:", error.response?.data || error.message);
+            res.status(500).json({ error: "Service Unavailable", details: error.message });
+        }
+    });
+    
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`API Gateway running on port ${PORT}`));
