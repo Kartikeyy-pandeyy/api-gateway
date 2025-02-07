c4e87f530dab6baf881bb9dd226050332a319b4a
Kartikeyy-pandeyy <kartikeyy.pandeyy@gmail.com>
2025-02-07 13:10:54 +0530

    updated for power

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ app.post("/calculate", async (req, res) => {
     else if(operation === "divide") {
         backendUrl = "https://calculator-production-e65e.up.railway.app/divide"
     }
+    else if(operation === "exponent") {
+        backendUrl = "https://calculator-production-e65e.up.railway.app/exponent"
+    }
     console.log("Request received:", { num1, num2, operation });
     console.log("Forwarding request to backend:", backendUrl);
 
